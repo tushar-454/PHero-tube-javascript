088d8d20a073f5ba8f9e51099eb1f4a0be833282
tushar-454 <imtushar454@gmail.com>
2023-08-31 22:42:26 +0600

    category section completed with resposiveness but not active color working after all main work finish I will fix this issue

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log('Hello world');
+// dynamic catagory create
+const dynamicCata = async () => {
+  const catagoryWraper = document.getElementById('catagoryWrper');
+  const promise = await fetch(
+    'https://openapi.programming-hero.com/api/videos/categories'
+  );
+  const response = await promise.json();
+  const categories = response.data;
+
+  for (const category of categories) {
+    const div = document.createElement('div');
+    div.innerHTML = `<button
+    class="bg-[#CBCBCB] rounded py-3 px-5 text-[#252525] text-xl font-medium active:bg-[#A5A5A5]">${category.category}</button>`;
+    catagoryWraper.append(div);
+    console.log(category.category);
+  }
+  console.log(categories);
+};
+dynamicCata();
